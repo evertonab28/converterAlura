5f79868c38f4c3259230e8388afef4ccdd9deb55
evertonab28 <42456197+evertonab28@users.noreply.github.com>
2023-11-14 19:34:49 -0400

    assets reorganization

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# converterAlura
+<h1 align="center"> Imersão DEV7 da Alura 💵💰 </h1>
+
+# 📃 About 
+This project was developed during Alura's Immersion Dev 7, where I made several significant improvements. Firstly, I introduced a home page that clearly displays all available converters. I also included a field so that the user can enter the value they want to convert, as well as buttons to perform the conversion and to clear the entered fields. As a class challenge, in addition to the dollar to Brazilian real converter, I implemented another one from euro to Brazilian real, and another spectacular one that converts distance from km to light years.
+
+# 🔍 Live test 
+Feel free to test the converter by clicking this [link](https://evertonab28.github.io/converterDev7Alura/)
+
+# 💻 View of interface
+
+<p align="center">
+  <img alt="View" src="/docs/assets/images/Converter.png" width="100%">
+</p>
+
+# 🔗 Technologies
+☑️ HTML and CSS
+☑️ JavaScript
---
+++ docs/assets/js/script.js
@@ -0,0 +1,77 @@
+var btnConvertDolar = document.getElementById('btn-convert-dolar');
+if (btnConvertDolar) {
+    btnConvertDolar.addEventListener('click', function () {
+        var inputValue = document.getElementById('money-input').value;
+        var resultDolar = convertDolar(inputValue);
+        var resultBTC = convertBTC(resultDolar);
+        var resultDiv = document.getElementById('result');
+        document.getElementById('result').innerHTML = 'Resultado:<br>' +'R$ ' + resultDolar.toFixed(2) +
+        '<br>' +
+        'Bitcon: ' + resultBTC.toFixed(7) + ' BTC';
+        resultDiv.style.display = 'flex';
+    });
+}
+
+var btnConvertEuro = document.getElementById('btn-convert-euro');
+if (btnConvertEuro) {
+    btnConvertEuro.addEventListener('click', function () {
+        var inputValue = document.getElementById('money-input').value;
+        var resultEuro = convertEuro(inputValue);
+        var resultBTC = convertBTC(resultEuro);
+        var resultDiv = document.getElementById('result');
+        document.getElementById('result').innerHTML = 'Resultado: R$ ' + resultEuro.toFixed(2) +
+        '<br>' +
+        'Bitcon: ' + resultBTC.toFixed(7) + ' BTC';
+        resultDiv.style.display = 'flex';
+    });
+}
+
+var btnConvertKm = document.getElementById('btn-convert-km');
+if (btnConvertKm) {
+    btnConvertKm.addEventListener('click', function () {
+        var inputValue = document.getElementById('km-input').value;
+        var resultKm = convertKm(inputValue);
+        var resultDiv = document.getElementById('result');
+        document.getElementById('result').innerHTML = 'Resultado: ' + resultKm + ' anos-luz';
+        resultDiv.style.display = 'flex';
+    });
+}
+
+var btnReset = document.getElementById('btnReset');
+if (btnReset) {
+    btnReset.addEventListener('click', function () {
+        var resultDiv = document.getElementById('result');
+        var inputField = document.getElementById('money-input');
+        resultDiv.innerHTML = '';
+        resultDiv.style.display = 'none';
+        inputField.value = '';
+    });
+}
+
+function convertDolar(value) {
+    const exchange = 4.86;
+    var convertedDolarBrl = value * exchange;
+    return convertedDolarBrl;
+}
+
+function convertEuro(value) {
+    const exchange = 5.29;
+    var convertedEuroBrl = value * exchange;
+    return convertedEuroBrl;
+}
+
+function convertBTC(value){
+    const exchange = 171573.26;
+    var convertedRealBTC = value / exchange;
+    return convertedRealBTC;
+}
+
+function convertKm(value){
+    const speedOfLight = 299792458;
+    const secondsPerYear = 365.25 * 24 * 60 * 60;
+    const distanceLightPerYear = speedOfLight * secondsPerYear;
+
+    const kmInMeters = value * 1000;
+    const lightYears = kmInMeters / distanceLightPerYear;
+    return lightYears;
+}
