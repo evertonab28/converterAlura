9c145dc1855769e83356f96fd967c58ebb4d1966
evertonab28 <42456197+evertonab28@users.noreply.github.com>
2023-11-14 15:37:37 -0400

    js changes and css adjustments

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-var valor;
-var cotacaoDolar = 4.90;
-var cotacaoEuro = 5.26;
+document.getElementById('btn-convert-dolar').addEventListener('click', function() {
+    var inputValue = document.getElementById('money-input').value;
+    var result = convertDolar(inputValue);
+    var resultDiv = document.getElementById('result');
+    document.getElementById('result').innerHTML = 'Resultado: R$ ' + result.toFixed(2);
+    resultDiv.style.display = 'flex';
+});
 
-var valorEmReal;
-valorEmReal = valorEmReal.toFixed(2);
+document.getElementById('btnReset').addEventListener('click', function() {
+    var resultDiv = document.getElementById('result');
+    var inputField = document.getElementById('money-input');
+    resultDiv.innerHTML = '';
+    resultDiv.style.display = 'none';
+    inputField.value = '';
+});
+
+function convertDolar(value) {
+    const exchange = 4.86;
+    var convertedDolarBrl = value * exchange;
+    return convertedDolarBrl;
+}
